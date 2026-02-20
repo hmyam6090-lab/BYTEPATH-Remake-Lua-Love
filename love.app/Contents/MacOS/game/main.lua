a206bcaf454d2b11c847dc27e59dca17fe0dfd9b
Yam <hmyam6090@gmail.com>
2026-02-19 19:23:13 -0500

    Object Oriented Programming Implementation in Love
    Used the rxi library for a basic OOP template and experimented with some basic objects

--- love.app/Contents/MacOS/game/main.lua
+++ love.app/Contents/MacOS/game/main.lua
@@ -1,14 +1,44 @@
 Object = require 'libraries/classic/classic'
 
+function CreateTable()
+    return {
+        a = 1,
+        b = 2,
+        c = 3,
+        sum = function(self) self.c = self.c + self.a + self.b end,
+    }
+end
+
 function love.load()
-    frame = 0
+    local object_files = {}
+    recursiveEnumerate('objects', object_files)
+    requireFiles(object_files)
+    hypecircle = HyperCircle(400, 300, 50, 10, 120)
+end
+
+function requireFiles(files)
+    for _, file in ipairs(files) do
+        local file = file:sub(1, -5)
+        require(file)
+    end
+end
+
+function recursiveEnumerate(folder, file_list)
+    local items = love.filesystem.getDirectoryItems(folder)
+    for _, item in ipairs(items) do
+        local file = folder .. '/' .. item
+        if love.filesystem.getInfo(file) then
+            table.insert(file_list, file)
+        elseif love.filesystem.isDirectory(file) then
+            recursiveEnumerate(file, file_list)
+        end
+    end
 end
 
 function love.update(dt)
-    frame = frame + 1
-    print(frame)
+    hypecircle:update(dt)
 end
 
 function love.draw()
-    love.graphics.print(frame, 400, 300)
+    hypecircle:draw()
 end
